750610171b8b64093ed2d096210eb3e248c98876
Tweecezor <thenamelessmanhd@gmail.com>
2021-06-06 02:35:03 +0300

    adsg

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = (env, argv) => {
 	const isProductionBuild = argv.mode === "production";
-	const publicPath = "/dist/vue-homework/";
+	const publicPath = "/vue-homework/dist/";
 
 	const pcss = {
 		test: /\.(p|post|)css$/,
